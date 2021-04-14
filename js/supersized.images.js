4bb28dbb3e1422bcdc06f0e70040e9a4b931ae10
Zafar201 <ayshasawaab@gmail.com>
2021-04-14 14:11:21 +0530

    3

--- js/supersized.images.js
+++ js/supersized.images.js
@@ -1,4 +1,6 @@
-			jQuery(function($){
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+		
+		jQuery(function($){
 				
 				$.supersized({
 				
